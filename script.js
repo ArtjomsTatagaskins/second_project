e4083f0dc3609056ed639e1cf4ce6c67a8849198
ArtjomsTatagaskins <Nova-III@yandex.ru>
2023-11-06 20:16:50 +0200

    Writing function for computer choice and round

--- script.js
+++ script.js
@@ -1,1 +1,19 @@
-console.log('hello');
+let computerSelection = getComputerChoice();
+let playerSelection = 'rock';
+
+function getComputerChoice() {
+    let variants = ['Rock', 'Paper', 'Scissors'];
+    let choice = variants[Math.floor(Math.random() * variants.length)];
+    return choice;
+}
+
+
+function playRound(playerSelection, computerSelection) {
+    let playerSelectionToLower = playerSelection.toLowerCase();
+    if (playerSelectionToLower === 'rock' && computerSelection === 'Paper') {
+        let result = 'You lose! Paper beats Rock';
+        return result;
+    }
+}
+
+console.log(playRound(playerSelection, getComputerChoice()));
